c6aa0c1c386616a081f233e7bd697e91a202c735
Lynn Simonen <lsimonen@my.wctc.edu>
2021-08-26 12:29:44 -0500

    5th try - on 1st-new-local-branch

--- Program.cs
+++ Program.cs
@@ -10,6 +10,7 @@ namespace FirstRemoteGitTry
             Console.WriteLine("This is my SECOND try at a remote git repository");
             Console.WriteLine("This is my third try... Commit All (ammend) ");
             Console.WriteLine("This is my FOURTH try... This change made on the remote GitHub repository... will need to PUSH these changes.");
+            Console.WriteLine("This is my 5th try... on 1st-new-local-branch");
         }
     }
 }
