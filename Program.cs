13ac902c4ff8338c4e1f7a44e2472a7767d20b97
Lynn Simonen <lsimonen@my.wctc.edu>
2021-08-26 11:33:55 -0500

    This is my first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FirstRemoteGitTry
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is my first try at a remote git repository");
+        }
+    }
+}
