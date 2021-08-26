88dcba424dc1a0be25f77bee847df5766a999d3f
lynnsimonen <79551770+lynnsimonen@users.noreply.github.com>
2021-08-26 12:11:15 -0500

    This is my FOURTH try
    This is my FOURTH try... This change made on the remote GitHub repository... will need to PUSH these changes.

--- Program.cs
+++ Program.cs
@@ -9,6 +9,7 @@ namespace FirstRemoteGitTry
             Console.WriteLine("This is my first try at a remote git repository");
             Console.WriteLine("This is my SECOND try at a remote git repository");
             Console.WriteLine("This is my third try... Commit All (ammend) ");
+            Console.WriteLine("This is my FOURTH try... This change made on the remote GitHub repository... will need to PUSH these changes.");
         }
     }
 }
